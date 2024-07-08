a191c034c077b0398b870f2a07b3ef9fb8c48333
elisiomualumene <elisiomualumene@gmail.com>
2024-07-08 16:17:54 +0100

    update category.repository.ts

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,8 +1,10 @@
 //storage -  armazenamento de dados
 
-export interface CategoryRepository {
-    
-}
+import { IRepository } from "@shared/domain/repository/repository-interface";
+import { Category } from "./category.entity";
+import { UUID } from "@shared/domain/value-objects/uuid.vo";
+
+export interface ICategoryRepository extends IRepository<Category, UUID> {}
 
 //memoria
 // sequelize
