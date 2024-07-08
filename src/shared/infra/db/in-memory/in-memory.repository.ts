13107ecbf0c208f44abbbee584b5bf72b10c93e9
elisiomualumene <elisiomualumene@gmail.com>
2024-07-08 16:18:14 +0100

    feat: create in-memory.repository.ts

---
+++ src/shared/infra/db/in-memory/in-memory.repository.ts
@@ -0,0 +1,127 @@
+import { NotFoundError } from "../../../domain/errors/not-found.error";
+import { Entity } from "../../../domain/entity";
+import {
+  IRepository,
+  ISearchableRepository,
+} from "../../../domain/repository/repository-interface";
+import { ValueObject } from "../../../domain/value-object";
+import {
+  SearchParams,
+  SortDirection,
+} from "../../../../shared/domain/repository/search-params";
+import { SearchResult } from "../../../../shared/domain/repository/search-result";
+
+export abstract class InMemoryRepository<
+  E extends Entity,
+  EntityId extends ValueObject
+> implements IRepository<E, EntityId>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+  async bulkInsert(entities: E[]): Promise<void> {
+    this.items.push(...entities);
+  }
+  async update(entity: E): Promise<void> {
+    const indexFound = this.items.findIndex((item) =>
+      item.entity_id.equals(entity.entity_id)
+    );
+    if (indexFound === -1) {
+      throw new NotFoundError(entity.entity_id, this.getEntity());
+    }
+    this.items[indexFound] = entity;
+  }
+  async delete(entity_id: EntityId): Promise<void> {
+    const indexFound = this.items.findIndex((item) =>
+      item.entity_id.equals(entity_id)
+    );
+    if (indexFound === -1) {
+      throw new NotFoundError(entity_id, this.getEntity());
+    }
+    this.items.splice(indexFound, 1);
+  }
+  async findById(entity_id: EntityId): Promise<E | null> {
+    const item = this.items.find((item) => item.entity_id.equals(entity_id));
+    return typeof item === "undefined" ? null : item;
+  }
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+  abstract getEntity(): new (...args: E[]) => E;
+}
+
+export abstract class InMemorySearchableRepository<
+    E extends Entity,
+    EntityId extends ValueObject,
+    Filter = string
+  >
+  extends InMemoryRepository<E, EntityId>
+  implements ISearchableRepository<E, EntityId, Filter>
+{
+  sortableFields: string[] = [];
+
+  async search(props: SearchParams<Filter>): Promise<SearchResult<E>> {
+    // Filter, Sort and Pagination
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemsSorted = this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sort_dir
+    );
+    const itemsPaginated = this.applyPagination(
+      itemsSorted,
+      props.page,
+      props.per_page
+    );
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      current_page: props.page,
+      per_page: props.per_page,
+    });
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: Filter | null
+  ): Promise<E[]>;
+
+  protected applySort(
+    items: E[],
+    sort: string | null,
+    sort_dir: SortDirection | null,
+    custom_getter?: (sort: string, item: E) => any
+  ) {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items;
+    }
+    return [...items].sort((a, b) => {
+      //@ts-ignore
+      const aValue = custom_getter ? custom_getter(sort, a) : a[sort];
+      //@ts-ignore
+      const bValue = custom_getter ? custom_getter(sort, b) : b[sort];
+      if (aValue < bValue) {
+        return sort_dir === "asc" ? -1 : 1;
+      }
+
+      if(aValue > bValue) {
+        return sort_dir === "asc" ? 1 : -1;
+      }
+
+      return 0;
+    });
+  }
+
+  protected applyPagination(
+    items: E[],
+    page: SearchParams["page"],
+    per_page: SearchParams["per_page"]
+  ) {
+    const start = (page - 1) * per_page; // 1 * 15 = 15
+    const limit = start + per_page; // 15 + 15 = 30
+    return items.slice(start, limit);
+  }
+}
