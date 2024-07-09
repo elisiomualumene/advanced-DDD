27561e2fcde1d4deaf906aa65279bddc81de9531
elisiomualumene <elisiomualumene@gmail.com>
2024-07-09 12:21:53 +0100

    update category-in-memory.repository.ts

--- src/category/infra/db/in-memory/category-in-memory.repository.ts
+++ src/category/infra/db/in-memory/category-in-memory.repository.ts
@@ -3,11 +3,12 @@ import { InMemorySearchableRepository } from "../../../../shared/infra/db/in-mem
 import { UUID } from "../../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../../../../category/domain/category.entity";
 import { SortDirection } from "../../../../shared/domain/repository/search-params";
+import { ICategoryRepository } from "../../../domain/category.repository";
 
 export class CategoryInMemoryRepository extends InMemorySearchableRepository<
   Category,
   UUID
-> {
+> implements ICategoryRepository {
   sortableFields: string[] = ["name", "created_at"];
 
   protected async applyFilter(
