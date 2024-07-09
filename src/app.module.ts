cbad241615d532e9f5e83fd2a4141bf5eaff3166
elisiomualumene <elisiomualumene@gmail.com>
2024-07-09 17:59:29 +0100

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: ':memory',
+      logging: false,
+      models: [CategoryModel],
+    }),
+    CategoriesModule,
+  ],
 })
 export class AppModule {}
