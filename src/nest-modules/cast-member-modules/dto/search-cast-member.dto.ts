bc83597c9119dabe76a0860548836d47bb2483cc
elisiomualumene <elisiomualumene@gmail.com>
2024-07-11 16:32:59 +0100

    create 7 files

---
+++ src/nest-modules/cast-member-modules/dto/search-cast-member.dto.ts
@@ -0,0 +1,4 @@
+import { ListCastMembersInput } from "@core/cast-member/application/use-cases/list-cast-member/list-cast-member.input";
+
+
+export class SearchCastMemberDto extends ListCastMembersInput {}
